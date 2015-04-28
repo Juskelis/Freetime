f23747995cdfa7baa8daaf9f4b0530c80b4a5ca4
ArgentumVir <ArgentumVir@users.noreply.github.com>
2015-04-28 16:49:14 -0700

    Update eServer.js

--- eServer.js
+++ eServer.js
@@ -1,3 +1,37 @@
-var express = require('express');
-var app = express();
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
 
+var message = [
+	'Server Running.'
+];
+
+http.createServer( function (req, res) {
+	res.setHeader('Content-Type', "text/html");
+	res.writeHead(200);
+
+	var filename = url.parse(req.url).pathname.slice(1);
+	
+	if (filename === '') {
+		filename = 'index.html';
+	}
+	
+	console.log('filename: ' + filename);
+	
+	fs.readFile(filename, 'utf8', function (err, data) {
+	  if (err) {
+		return console.log(err);
+	  }
+	  res.end(data);
+	});
+	
+}).listen(8080);
+	
+	fs.readFile(filename, 'utf8', function (err, data) {
+	  if (err) {
+		return console.log(err);
+	  }
+	  res.end(data);
+	});
+	
+}).listen(8080);

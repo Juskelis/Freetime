f61f296fa1011f6299d81deffe03375a25ef7e71
JXVD <jackvangame@live.com>
2015-06-01 19:05:36 -0700

    Server stuff updated

--- createFTData.js
+++ createFTData.js
@@ -2,24 +2,15 @@
 	db.createCollection('users')
 	userCollection = db.getCollection("users")
 	userCollection.remove({})
-	userCollection.insert(
-	{
-		  name: "Fred Time",
-		  uID:1,
-		  calendarId: 1,
-		  fCalendarIDs[
-		  {
-			fID: 2
-		  ]
-	}
-	)
 	
 	userCollection.insert(
 	{  
+		  //jackson
 		  name: "Francis Time",
-		  uID: 2,
+		  uID: 10204287096954473,
 		  calendarID: 3,
-		  fCalendarIDs[
+		  hashedPW: "n4bQgYhMfWWaL+qgxVrQFaO/TxsrC4Is0V1sFbDwCgg=",
+		  fCalendarIDs:[
 		  {
 			  fID:1
 		  },
@@ -27,21 +18,43 @@
 			  fID:2
 		  }
 		  ]
-	
 	}
 	)
+	userCollection.insert(
+	{
+		  name: "Fred Time",
+		  uID:1,
+		  calendarId: 1,
+		  hashedPW: "n4bQgYhMfWWaL+qgxVrQFaO/TxsrC4Is0V1sFbDwCgg=",
+		  fCalendarIDs:[
+		  {
+			fID: 2
+		  }
+		  ]
+	}
+	)
+	
+	
 	userCollection.insert(
 	{  
 		  name: "Frank Time",
 		  uID: 3,
 		  calendarID: 2,
-		  fCalendarIDs[
+		  hashedPW: "n4bQgYhMfWWaL+qgxVrQFaO/TxsrC4Is0V1sFbDwCgg=",
+		  fCalendarIDs:[{
+		  }
 		  ]
 	}
 	)
 
 
-
+user1 = userCollection.findOne({name: 'Francis Time'})
+user1Id = user1._id.valueOf();
+user2 = userCollection.findOne({username: 'Fred Time'})
+user2Id = user2._id.valueOf();
+user3 = userCollection.findOne({username: 'Frank'})
+user3Id = user3._id.valueOf();	
+	
 
 	db.createCollection('calendars')
 	calendarsCollection = db.getCollection("calendars")
@@ -150,7 +163,7 @@
 		 },
 		 {
 			title: "Free Tim",
-			description: "Gotta get my homie free",
+			description: "Gotta get my homie free, Gucci too",
 			id: 24,
 			privacy: "true",
 			start: "2015-05-12T16:00-23:00"
--- ftMongoServ.js
+++ ftMongoServ.js
@@ -1,8 +1,19 @@
-var express = require('express');
-var url = require('url');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser')
+var express = require('express')
+  , passport = require('passport')
+  , util = require('util')
+  , ejs = require('ejs')
+  , http = require('http')
+  , morgan = require('morgan')
+  , bodyParser = require('body-parser')
+  , methodOverride = require('method-override')
+  , cookieParser = require('cookie-parser')
+  , session = require('express-session')
+  , FacebookStrategy = require('passport-facebook').Strategy;
 var app = express();
+
+var FACEBOOK_APP_ID = "372279769646000"; 
+var FACEBOOK_APP_SECRET = "b70bc4f287f6648696feed2a3feaf25b";
+
 
 // create application/json parser
 var jsonParser = bodyParser.json();
@@ -13,28 +24,83 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var Users;
 var Calendars;
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
 
 //put config url behind file to hide passwords and username
 var mongoDBConnection = require('./db.ftSample.config');
 
 console.log(mongoDBConnection.uri);
 
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+	callbackURL: "http://me.localtest.me:3000/auth/facebook/callback"
+  },
+  function	(accessToken, refreshToken, profile, done) {
+    //User.find({uId: profile.id}, function(err, user) {
+    //if (err) { return done(err); }
+	process.nextTick(function () {
+      
+    return done(null, profile);
+    });
+  }
+));
+
+
+
+var server = http.createServer(app);
+
+
+// configure Express
+//app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.engine('html', ejs.renderFile);
+  //  app.use(express.logger());
+  app.use(morgan('combined'));
+  app.use(cookieParser());
+  app.use(bodyParser.urlencoded({extend: false}));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+  app.use(session({ secret: 'keyboard cat' }));
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(express.Router());
+  //app.use(express.static(__dirname + '/public'));
+//});
+
+
+
+
+
+
+
 mongoose.connect(mongoDBConnection.uri);
 mongoose.connection.on('open', function() {
 	var Schema = mongoose.Schema;
-	var UserSchema = new Schema( 
+	var userSchema = new Schema( 
 		{
 			name: String,
 			uID: Number,
 			calendarId: Number,
+			hashedPW: String,
 			fcalendarIDs[{
 				fID: Number
 			}
 			]
+
 		},
 	   {collection: 'users'}
 	);
-	Users = mongoose.model('Users', UserSchema);
+	Users = mongoose.model('Users', userSchema);
 	
 	var calendarSchema = new Schema( 
 		{
@@ -52,18 +118,172 @@ mongoose.connection.on('open', function() {
 	);
 	
 	
-	Calendars = mongoose.model('Calendars', calendarIdSchema);
+	Calendars = mongoose.model('Calendars', calendarSchema);
 	console.log('models have been created');
 });
+//------------------------------------------------------------------------------------------------------------------------------
 
+// GET /auth/facebook 
+//   Use passport.authenticate() as route middleware to authenticate the 
+//   request.  The first step in Facebook authentication will involve 
+//   redirecting the user to facebook.com.  After authorization, Facebook will 
+//   redirect the user back to this application at /auth/facebook/callback 
+app.get('/auth/facebook',
+  passport.authenticate('facebook', { scope: ['public_profile'] }),
+  function(req, res){
+    // The request will be redirected to Facebook for authentication, so
+    // this function will not be called.
+  });
 
-function getAllEvents(res){
-	console.log("inside get all events");
-	var query = Events.find({});
-	query.exec(function (err, itemArray) {
- 		res.json(itemArray);
+// GET /auth/facebook/callback 
+//   Use passport.authenticate() as route middleware to authenticate the 
+//   request.  If authentication fails, the user will be redirected back to the 
+//   login page.  Otherwise, the primary route function function will be called, 
+//   which, in this example, will redirect the user to the home page. 
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+  
+  
+ //FUNCTIONS
+ //-----------------------------------------------------------------------------------------------------------
+ 
+ //User has their own calendar with ID, find that
+ //then find that calendar
+ //then find the specific  event that needs to be changed
+ function DeleteEvent(req, res, eID) {
+	var curID = req.session.user.id;
+	var calQuery = Users.findOne({uID: curID});
+	calQuery.exec(function(err,user) {
+		if(!err) {
+			var calID = Users.calendarId;
+			var query = Calendars.update({calendarId: calID}, {$pull: { events:{id: eID}}});
+			query.exec(function(err, numOfDocsChanged) {
+				console.log("Changed " + numOfDocsChanged);
+			});
+		}
 	});
+	/*
+	var query = Calendars.update({calendarId: calID}, {$pull: { events:{id: eID}}});
+	query.exec(function(err, numOfDocsChanged) {
+		console.log("Changed " + numOfDocsChanged);
+	});
+	*/
 }
+
+//I think this might be better handled without a function, just in the call proper
+function UpdateEvent(res, userID, eID, event) {
+
+	//gets the calendarID from the user
+	var curID = req.session.user.id;
+	var calQuery = Users.find({uID: curID});
+	calQuery.exec(function(err, user) {
+		if(!err) {
+			var calID = user.calendarId;
+			/*
+				performs the update on the given eID in the given calID
+			*/
+		}
+	});
+	
+	/*
+	var query = Calendars.update(
+		{calendarId: calID, events.id: eID},
+		{$set:
+			{
+			}
+		}
+	);
+	*/
+}
+
+ function getMyEvents(req, res){
+	 curId = req.user.id;
+	 query = Calendars.findOne({uID: curID});
+	 query.exec(function (err, itemArray) {
+		displayDBError(err);
+		console.log("result: " + itemArray);
+		res.json(itemArray);
+	});
+ }
+ 
+ function getFriendEvents(req,res){
+	 curID = req.session.user.id;
+	 query = Calendars.find({events}).where('calId').in(Users.fcalendarIDs);
+	 query.exec(function (err, itemArray) {
+		displayDBError(err);
+		console.log("result: " + itemArray);
+		res.json(itemArray);
+	});
+ }	
+
+app.use('/', express.static('./apps/'));
+app.use('/eventSources/', express.static('./eventsources'));	
+	
+
+//-----------API's----------------------------------------------------------------------
+	
+ //loading calendars
+app.get('/cal/', function (req, res){
+	var id = req.params.uID;
+	console.log("get all events");
+	getAllEvents(res, uID: id);
+});
+
+app.get('/cal/self',function (req, res){
+	var id = req.params.uID;
+	console.log("get all events");
+	getAllEvents(res, uID: id);
+});
+
+app.get('/cal/friends',function (req, res){
+	var id = req.params.uID;
+	console.log("get all events");
+	getAllEvents(res, uID: id);
+});
+
+//loading and editing events
+/*
+app.put('/app/lists/:listId', jsonParser, function(req, res) {
+	var id = req.params.listId;
+	var jsObj = req.body;
+	jsObj.owner = req.session.user;
+	Lists.update({listId: id}, jsObj, {multi: false}, function (err) {
+		if (err) {
+			console.log('object update failed');
+		}
+	});
+	res.sendStatus(200);
+});
+*/
+
+app.put('/event/:eID', jsonParser, function (req, res){
+	var eventId = req.params.eID;
+	
+	Calendars.update({
+	
+});
+ 
+ app.delete('/event/:eID', jsonParser, function (req, res){
+	 
+});
+ 
+ 
+ 
+ 
+app.listen(80);
+ 
+ //-----------------------------------------------------------------------------------------------------------
+ 
+ 
+ 
+ 
+ 
+ 
+/*
 
 function getMyEvents(res){
 	
@@ -74,6 +294,7 @@ function getMyEvents(res){
 	});
 }
 
+
 function getFriendEvents(res){
 	console.log("inside get friend events");
 	var query = Events.find({eventListId:2});
@@ -82,17 +303,35 @@ function getFriendEvents(res){
 	});
 }
 
+function getMyEvents(res, query){
+	console.log("inside getSingleUserEvents")
+	var query = Calendars.findOne({Users.findOne({calendarId:query})});
+	query.exec(function (err, itemArray) {
+		res.json(itemArray);		
+	});
+}
+
+function getAllEvents(res, query){
+	console.log("inside get all events");
+	var query =Calendars.find({Users.find);
+	query.exec(function (err, itemArray) {
+ 		res.json(itemArray);
+	});
+}
 
 
-app.use('/', express.static('./apps/'));
-app.use('/eventSources/', express.static('./eventsources'));
-
-
-app.get('/:uId/', function(req,res){
+//how hilerio calls for list ID: <h4 class="panel-heading"><a href="#/lists/{{result.listId}}">{{$index + 1}} - {{result.name}}</a></h4>
+app.get('/:uID/', function(req,res){
+	var id = req.params.uID;
+	console.log("get" + id + "'s events");
+	getSingleUserEvents(res, {uID: id});
 });
+
+
 app.get('/events/', function (req, res){
+	var id = req.params.uID;
 	console.log("get all events");
-	getAllEvents(res);
+	getAllEvents(res, uID: id);
 });
 
 app.get('/events/self', function (req, res){
@@ -105,5 +344,4 @@ app.get('/events/friend', function (req, res){
 	getFriendEvents(res);
 });
 
-
-app.listen(80);
+*/

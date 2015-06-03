01089ac074fb355377fdb2463b25954f266f187b
Matt Juskelis <matthewjuskelis@gmail.com>
2015-06-03 13:49:00 -0700

    Fixed eventController

--- apps/Modules/Event/Controllers/eventController.js
+++ apps/Modules/Event/Controllers/eventController.js
@@ -30,11 +30,10 @@ angular
 			}
 		*/
 		$scope.submitEvent = function(event) {
-			if($scope.event.title.length > 0
-				&& $scope.event.startDate.length > 0)
+			if($scope.event.title.length > 0)
 			{
 				if($rootScope.event != null) {
-					$http.put('/event/$scope.event.id',event).success(function(data, status, headers, config) {
+					$http.put('/event/' + $scope.event.id,event).success(function(data, status, headers, config) {
 						$location.path('calendar');
 					});
 				}
@@ -48,7 +47,7 @@ angular
 		
 		$scope.deleteEvent = function(event) {
 			//no validation required
-			$http.delete('/event/$scope.event.id', event).success(function(data,status,headers,config) {
+			$http.delete('/event/' + $scope.event.id, event).success(function(data,status,headers,config) {
 				$location.path('calendar')
 			});
 		};
